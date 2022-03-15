5bc9d88cd044b683ed0855721461f0b32f82b4ba
Odey, Samuel Michael <sammic297@gmail.com>
2022-03-15 19:28:41 +0100

    Update deploy.js

--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -3,33 +3,33 @@ require("dotenv").config({ path: ".env" });
 const { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS } = require("../constants");
 
 async function main() {
-  // Address of the Crypto Devs NFT contract that you deployed in the previous module
-  const cryptoDevsNFTContract = CRYPTO_DEVS_NFT_CONTRACT_ADDRESS;
+    // Address of the Crypto Devs NFT contract that you deployed in the previous module
+    const cryptoDevsNFTContract = CRYPTO_DEVS_NFT_CONTRACT_ADDRESS;
 
-  /*
-    A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
-    so cryptoDevsTokenContract here is a factory for instances of our CryptoDevToken contract.
-    */
-  const cryptoDevsTokenContract = await ethers.getContractFactory(
-    "CryptoDevToken"
-  );
+    /*
+      A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+      so cryptoDevsTokenContract here is a factory for instances of our CryptoDevToken contract.
+      */
+    const cryptoDevsTokenContract = await ethers.getContractFactory(
+        "CryptoDevToken"
+    );
 
-  // deploy the contract
-  const deployedCryptoDevsTokenContract = await cryptoDevsTokenContract.deploy(
-    cryptoDevsNFTContract
-  );
+    // deploy the contract
+    const deployedCryptoDevsTokenContract = await cryptoDevsTokenContract.deploy(
+        cryptoDevsNFTContract
+    );
 
-  // print the address of the deployed contract
-  console.log(
-    "Crypto Devs Token Contract Address:",
-    deployedCryptoDevsTokenContract.address
-  );
+    // print the address of the deployed contract
+    console.log(
+        "Crypto Devs Token Contract Address:",
+        deployedCryptoDevsTokenContract.address
+    );
 }
 
 // Call the main function and catch if there is any error
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
